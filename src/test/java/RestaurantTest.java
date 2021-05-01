06292e6b170eb8d297fb16217499d97f42418c5f
shivakarthikan@gmail.com <shivakarthikan@gmail.com>
2021-05-01 21:16:35 +0530

    Part 3: Failing test case

--- src/test/java/RestaurantTest.java
+++ src/test/java/RestaurantTest.java
@@ -80,26 +80,23 @@ class RestaurantTest {
 
 	// <<<<<<<<<<<<<<<<<<<<<<<Order>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-	/*
-	 * @Test void adding_item_should_give_correct_order_value() { restaurant = new
-	 * Restaurant("Amelie's cafe", "Chennai", openingTime, closingTime);
-	 * restaurant.addToMenu("Sweet corn soup", 119);
-	 * restaurant.addToMenu("Vegetable lasagne", 269); menuItems = new
-	 * ArrayList<>(); menuItems.add("Sweet corn soup");
-	 * menuItems.add("Vegetable lasagne"); assertEquals(388,
-	 * restaurant.orderValue(menuItems)); }
-	 * 
-	 * @Test void adding_zero_items_to_the_menu_should_give_correct_order_value() {
-	 * restaurant = new Restaurant("Amelie's cafe", "Chennai", openingTime,
-	 * closingTime); menuItems = new ArrayList<>(); assertEquals(0,
-	 * restaurant.orderValue(menuItems)); }
-	 * 
-	 * @Test void orderValue_should_always_return_zero() { restaurant = new
-	 * Restaurant("Amelie's cafe", "Chennai", openingTime, closingTime);
-	 * restaurant.addToMenu("Sweet corn soup", 119);
-	 * restaurant.addToMenu("Vegetable lasagne", 269); menuItems = new
-	 * ArrayList<>(); menuItems.add("Sweet corn soup");
-	 * menuItems.add("Vegetable lasagne"); assertEquals(0,
-	 * restaurant.orderValue(menuItems)); }
-	 */
+
+
+	@Test
+	void adding_zero_items_to_the_menu_should_give_correct_order_value() {
+		restaurant = new Restaurant("Amelie's cafe", "Chennai", openingTime, closingTime);
+		menuItems = new ArrayList<>();
+		assertEquals(0, restaurant.orderValue(menuItems));
+	}
+
+	@Test
+	void orderValue_should_always_return_zero() {
+		restaurant = new Restaurant("Amelie's cafe", "Chennai", openingTime, closingTime);
+		restaurant.addToMenu("Sweet corn soup", 119);
+		restaurant.addToMenu("Vegetable lasagne", 269);
+		menuItems = new ArrayList<>();
+		menuItems.add("Sweet corn soup");
+		menuItems.add("Vegetable lasagne");
+		assertEquals(0, restaurant.orderValue(menuItems));
+	}
 }
